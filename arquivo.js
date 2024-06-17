08b2d06e02530f430add6c4c259aef921b86fa2a
Vinicius <99505772+vinicius-alves-oliveira@users.noreply.github.com>
2024-06-17 11:33:25 -0300

    primeiro commit aula-github

---
+++ arquivo.js
@@ -0,0 +1,11 @@
+function media(num1,num2){
+    
+    var media = (num1+ num2)/2;
+
+    if(media >= 7){
+        console.log("Aprovado!");
+    }
+    else{
+        console.log("Reprovado!");
+    }
+}
